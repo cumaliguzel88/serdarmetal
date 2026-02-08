b1e1542bce1ab33942031680babf2fc552350e12
CUMALİ GÜZEL <cumali.guzel@std.yeditepe.edu.tr>
2026-02-08 16:25:23 +0300

    fix: update address and remove chrome nickel from about text

--- src/content/site.config.ts
+++ src/content/site.config.ts
@@ -11,12 +11,12 @@ export const siteConfig = {
     locations: [
       {
         id: 'tekirdag-ergene',
-        label: 'Velimeşe Yulaflı OSB Tekirdağ/Ergene',
-        address: 'Velimeşe Yulaflı OSB Tekirdağ/Ergene',
+        label: 'Yulaflı Mahallesi Hasan Tahsin Cad. 70/1 Ergene Tekirdağ',
+        address: 'Yulaflı Mahallesi Hasan Tahsin Cad. 70/1 Ergene Tekirdağ',
         mapOpenUrl:
-          'https://www.google.com/maps/search/?api=1&query=Velime%C5%9Fe+Yulafl%C4%B1+OSB+Tekirda%C4%9F%2FErgene',
+          'https://www.google.com/maps/search/?api=1&query=Yulafl%C4%B1+Mahallesi+Hasan+Tahsin+Cad.+70%2F1+Ergene+Tekirda%C4%9F',
         mapEmbedUrl:
-          'https://maps.google.com/maps?q=Velime%C5%9Fe%20Yulafl%C4%B1%20OSB%20Tekirda%C4%9F%2FErgene&t=&z=13&ie=UTF8&iwloc=&output=embed'
+          'https://maps.google.com/maps?q=Yulafl%C4%B1%20Mahallesi%20Hasan%20Tahsin%20Cad.%2070%2F1%20Ergene%20Tekirda%C4%9F&t=&z=15&ie=UTF8&iwloc=&output=embed'
       }
     ]
   },
