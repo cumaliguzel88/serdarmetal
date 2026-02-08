e64f30935360276c45f491cb4c875282d8b7d585
CUMALİ GÜZEL <cumali.guzel@std.yeditepe.edu.tr>
2026-02-08 16:39:38 +0300

    fix: pin map links to exact address coordinates

--- src/content/site.config.ts
+++ src/content/site.config.ts
@@ -14,9 +14,9 @@ export const siteConfig = {
         label: 'Yulaflı Mahallesi Hasan Tahsin Cad. 70/1 Ergene Tekirdağ',
         address: 'Yulaflı Mahallesi Hasan Tahsin Cad. 70/1 Ergene Tekirdağ',
         mapOpenUrl:
-          'https://www.google.com/maps/search/?api=1&query=Yulafl%C4%B1+Mahallesi+Hasan+Tahsin+Cad.+70%2F1+Ergene+Tekirda%C4%9F',
+          'https://www.google.com/maps/search/?api=1&query=41.211351412913%2C27.823096433572',
         mapEmbedUrl:
-          'https://maps.google.com/maps?q=Yulafl%C4%B1%20Mahallesi%20Hasan%20Tahsin%20Cad.%2070%2F1%20Ergene%20Tekirda%C4%9F&t=&z=15&ie=UTF8&iwloc=&output=embed'
+          'https://maps.google.com/maps?q=41.211351412913%2C27.823096433572&t=&z=17&ie=UTF8&iwloc=&output=embed'
       }
     ]
   },
